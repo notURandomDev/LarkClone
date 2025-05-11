1c3071469244e535b8c93ed2ab426e77a2c50c93
“notURandomDev” <1250901577@qq.com>
2025-05-11 20:44:38 +0800

    fix：恢复误删的头文件RelativeDateFormatter.h

---
+++ LarkClone/Frameworks/UI/Components/RelativeDateFormatter.h
@@ -0,0 +1,17 @@
+//
+//  RelativeDateFormatter.h
+//  LarkClone
+//
+//  Created by 张纪龙 on 2025/5/10.
+//
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RelativeDateFormatter : NSObject
+
+- (NSString *)stringFromDate:(NSDate *)date;
+
+@end
+
+NS_ASSUME_NONNULL_END
